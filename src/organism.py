925426022849c721fcf1ebe73523d988bab25a71
Erik <erikejan@hotmail.com>
2017-05-16 07:54:29 +0200

    Organisms now moving

---
+++ src/organism.py
@@ -0,0 +1,9 @@
+class Organism:
+    
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.v_x, self.v_y = 100.0, 100.0
+
+    def update(self, dt):
+        self.x += self.v_x * dt 
+        self.y += self.v_y * dt
--- src/stim.py
+++ src/stim.py
@@ -2,18 +2,28 @@ import pyglet
 from pyglet.gl import *
 from math import pi, sin, cos
 import draw
+from organism import Organism
 
 window = pyglet.window.Window(800, 600)
 
-glClearColor(0.2, 0.4, 0.5, 1.0)
+glClearColor(0.1, 0.2, 0.3, 1)
+
+organisms = [Organism(100, 120), Organism(200, 300)]
 
 @window.event
 def on_draw():
+    window.clear()
     glClear(GL_COLOR_BUFFER_BIT)
-
     glColor3f(0, 0, 0)
+    for o in organisms:
+        draw.predator(o)
 
-    draw.circle(200, 200, 100)
+def update(dt):
+    draw.circle(100,100,100)
+    for o in organisms:
+        o.update(dt)
+
+pyglet.clock.schedule_interval(update, 1/120.0)
 
 if __name__ == '__main__':
     pyglet.app.run()
